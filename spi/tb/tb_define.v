c827280b0621e179b26a5c87c65fd9e65aca79d5
zwm <verilog@aliyun.com>
2020-09-21 23:32:07 -0400

    modify tb

--- spi/tb/tb_define.v
+++ spi/tb/tb_define.v
@@ -2,22 +2,8 @@
 `define DUMP_FSDB               1
 //`define DUMP_ARRAY              1
 
-//`define TEST_AFC
 
-`define TEST_LOGEN
-
-
-`define CASE_START              0
-//`define CASE_START              201             // AFC RX: 0~201
-//`define CASE_START              404             // AAC: 404~404+31
-`define CASE_STOP               (404 + 31)
-
-`define LOG_DIR                 "../log"
-`define FILE_CASE_LIST          "./case_list"
-`define FILE_CFG                "cfg.txt"
-`define NCNTR_M19               "ncntr_m19.txt"
-`define NCNTR_M64               "ncntr_m64.txt"
-`define AFC_TOP                 tb_top.u_afc
+`define SPI_MASTER_TOP          tb_top.u_spi_master
 
 
 
--- spi/tb/tb_top.v
+++ spi/tb/tb_top.v
@@ -3,76 +3,45 @@ module tb_top();
 // macro
 `include "tb_define.v"
 // port
-reg                         rstn;
-reg                         clk;
-reg                         afc_cg_auto;
-reg                         afc_start;
-reg                         cdb_afc_start;
-reg                         cdb_bypass;
-reg                         trx;
-reg         [15:0]          divr;
-reg                         rg_forceband_en;
-reg         [6:0]           rg_vco_capband;
-reg         [1:0]           rg_afc_vcostable_time;
-reg         [6:0]           rg_afc_cnt_time;
-reg         [13:0]          a2d_afc_ncntr;
-reg                         rg_aac_bypass;
-reg         [1:0]           rg_aac_stable_time;
-reg         [1:0]           rg_aac_cbandrange;
-reg         [3:0]           rg_ini_ibsel_rx; // aac_ibvco initial val
-reg         [3:0]           rg_ini_ibsel_tx; // aac_ibvco initial val
-reg         [1:0]           rg_aac_vrefsel_rx;
-reg         [1:0]           rg_aac_vrefsel_tx;
-reg                         a2d_aac_pkd_state;
-wire                        afc_openloop_en;
-wire        [6:0]           afc_vco_capband;
-wire                        afc_cntr_rstn;
-wire                        afc_cntr_en;
-wire                        afc_cntr_datasyn;
-wire        [3:0]           aac_ibvco;
-wire                        aac_comp_en;
-wire        [1:0]           aac_comp_vref;
-wire        [13:0]          afc_minerr;
-wire                        afc_finish;
-// logen
-reg                         logen_rstn;   // logen_rstn,
-reg                         logen_start;
-reg                         rg_logen_cal_bypass;
-reg         [1:0]           rg_logen_cnt_sel;
-reg         [2:0]           rg_logen_vsel_man;  // mannual mode
-reg         [2:0]           rg_logen_vsel_seg0;
-reg         [2:0]           rg_logen_vsel_seg1;
-reg         [2:0]           rg_logen_vsel_seg2;
-reg         [2:0]           rg_logen_vsel_seg3;
-reg         [2:0]           rg_logen_vsel_seg4;
-reg         [5:0]           rg_logen_cntr_bound0;
-reg         [5:0]           rg_logen_cntr_bound1;
-reg         [5:0]           rg_logen_cntr_bound2;
-reg         [5:0]           rg_logen_cntr_bound3;
-wire                        logen_cntr_rstn;
-wire                        logen_cntr_en;
-wire                        logen_cntr_datasyn;
-wire        [5:0]           logen_cntr_curr;
-wire        [2:0]           ldo_logen_vsel;
+reg                             rstn;
+reg                             clk;
+reg                             tx_buf_vld; // one data byte valid to be transmitted
+reg         [7:0]               tx_buf_byte;
+reg                             rx_buf_vld; // rx buffer is empty to receive one byte
+reg                             spi_en;
+reg                             spi_start_pulse;
+reg                             tx_rx_seq;
+reg         [19:0]              tx_len;
+reg         [19:0]              rx_len;
+reg         [3 :0]              clk_div;
+reg                             cpha;
+reg                             cpol;
+reg                             rx_adj_en;
+reg         [3 :0]              rx_adj_clk;
+reg         [2 :0]              ncs_dly;
+wire                            miso;
+wire                            tx_buf_req;
+wire                            rx_buf_req;
+wire        [7:0]               rx_buf_byte;
+wire                            ncs;
+wire                            sck;
+wire                            mosi;
+wire        [7:0]               spi_status;
 // global
 integer err_cnt, chk_cnt, case_num;
 reg [48*8-1:0] log_dir;
-reg         [6:0]           target_vco_capband;
-reg [13:0] ncntr_table [255:0];
+
+// loopback
+assign miso = mosi;
 
 // main
 initial begin
+    // init
     sys_init;
     #1_000;
 
-`ifdef TEST_AFC
-    // start sim
+    // main
     main_loop;
-`endif
-
-`ifdef TEST_LOGEN
-    logen_test;
-`endif
 
     // disp
     #1_000;
@@ -83,108 +52,32 @@ initial begin
 end
 
 // inst
-afc u_afc (
+spi_master u_spi_master (
     .rstn                       ( rstn                          ),
     .clk                        ( clk                           ),
-    .afc_cg_auto                ( afc_cg_auto                   ),
-    .afc_start                  ( afc_start                     ),
-    .cdb_afc_start              ( cdb_afc_start                 ),
-    .cdb_bypass                 ( cdb_bypass                    ),
-    .trx                        ( trx                           ),
-    .divr                       ( divr                          ),
-    .rg_forceband_en            ( rg_forceband_en               ),
-    .rg_vco_capband             ( rg_vco_capband                ),
-    .rg_afc_vcostable_time      ( rg_afc_vcostable_time         ),
-    .rg_afc_cnt_time            ( rg_afc_cnt_time               ),
-    .a2d_afc_ncntr              ( a2d_afc_ncntr                 ),
-    .rg_aac_bypass              ( rg_aac_bypass                 ),
-    .rg_aac_stable_time         ( rg_aac_stable_time            ),
-    .rg_aac_cbandrange          ( rg_aac_cbandrange             ),
-    .rg_ini_ibsel_rx            ( rg_ini_ibsel_rx               ),
-    .rg_ini_ibsel_tx            ( rg_ini_ibsel_tx               ),
-    .rg_aac_vrefsel_tx          ( rg_aac_vrefsel_tx             ),
-    .rg_aac_vrefsel_rx          ( rg_aac_vrefsel_rx             ),
-    .a2d_aac_pkd_state          ( a2d_aac_pkd_state             ),
-    .afc_openloop_en            ( afc_openloop_en               ),
-    .afc_vco_capband            ( afc_vco_capband               ),
-    .afc_cntr_rstn              ( afc_cntr_rstn                 ),
-    .afc_cntr_en                ( afc_cntr_en                   ),
-    .afc_cntr_datasyn           ( afc_cntr_datasyn              ),
-    .aac_ibvco                  ( aac_ibvco                     ),
-    .aac_comp_en                ( aac_comp_en                   ),
-    .aac_comp_vref              ( aac_comp_vref                 ),
-    .afc_minerr                 ( afc_minerr                    ),
-    .afc_finish                 ( afc_finish                    )
+    .tx_buf_vld                 ( tx_buf_vld                    ),
+    .tx_buf_byte                ( tx_buf_byte                   ),
+    .tx_buf_req                 ( tx_buf_req                    ),
+    .rx_buf_req                 ( rx_buf_req                    ),
+    .rx_buf_byte                ( rx_buf_byte                   ),
+    .rx_buf_vld                 ( rx_buf_vld                    ),
+    .spi_en                     ( spi_en                        ),
+    .spi_start_pulse            ( spi_start_pulse               ),
+    .tx_rx_seq                  ( tx_rx_seq                     ),
+    .tx_len                     ( tx_len                        ),
+    .rx_len                     ( rx_len                        ),
+    .clk_div                    ( clk_div                       ),
+    .cpha                       ( cpha                          ),
+    .cpol                       ( cpol                          ),
+    .rx_adj_en                  ( rx_adj_en                     ),
+    .rx_adj_clk                 ( rx_adj_clk                    ),
+    .ncs_dly                    ( ncs_dly                       ),
+    .ncs                        ( ncs                           ),
+    .sck                        ( sck                           ),
+    .mosi                       ( mosi                          ),
+    .miso                       ( miso                          ),
+    .spi_status                 ( spi_status                    )
 );
-
-// u_logen
-logen_process_cal u_logen (
-    .rstn                       ( logen_rstn                    ),
-    .clk                        ( clk                           ),
-    .logen_start                ( logen_start                   ),
-    .rg_logen_cal_bypass        ( rg_logen_cal_bypass           ),
-    .rg_logen_cnt_sel           ( rg_logen_cnt_sel              ),
-    .rg_logen_vsel_man          ( rg_logen_vsel_man             ),
-    .rg_logen_vsel_seg0         ( rg_logen_vsel_seg0            ),
-    .rg_logen_vsel_seg1         ( rg_logen_vsel_seg1            ),
-    .rg_logen_vsel_seg2         ( rg_logen_vsel_seg2            ),
-    .rg_logen_vsel_seg3         ( rg_logen_vsel_seg3            ),
-    .rg_logen_vsel_seg4         ( rg_logen_vsel_seg4            ),
-    .rg_logen_cntr_bound0       ( rg_logen_cntr_bound0          ),
-    .rg_logen_cntr_bound1       ( rg_logen_cntr_bound1          ),
-    .rg_logen_cntr_bound2       ( rg_logen_cntr_bound2          ),
-    .rg_logen_cntr_bound3       ( rg_logen_cntr_bound3          ),
-    .a2d_ncntr                  ( a2d_afc_ncntr                 ),
-    .cntr_rstn                  ( logen_cntr_rstn               ),
-    .cntr_en                    ( logen_cntr_en                 ),
-    .cntr_datasyn               ( logen_cntr_datasyn            ),
-    .logen_cntr_curr            ( logen_cntr_curr               ),
-    .ldo_logen_vsel             ( ldo_logen_vsel                )
-);
-
-wire cntr_rstn      = afc_cntr_rstn & logen_cntr_rstn;
-wire cntr_en        = afc_cntr_en | logen_cntr_en;
-wire cntr_datasyn   = afc_cntr_datasyn | logen_cntr_datasyn;
-
-// ncntr logic
-wire [7:0] ncntr_index = {trx, afc_vco_capband};
-always @(posedge clk or negedge rstn)
-    if (~rstn)
-        a2d_afc_ncntr <= {$random}%(2**14);
-    else if (~cntr_rstn)
-        a2d_afc_ncntr <= {$random}%(2**14);
-    else if (cntr_datasyn)
-        a2d_afc_ncntr <= ncntr_table[ncntr_index];
-
-// a2d_aac_pkd_state
-reg [3:0] a2d_cnt;
-reg [3:0] a2d_tmp;
-always @(posedge clk or negedge rstn)
-    if (~rstn) begin
-        a2d_aac_pkd_state <= 0;
-        a2d_cnt <= 0;
-    end
-    else if (`AFC_TOP.st_curr == 9) begin
-        //a2d_tmp = {$random}%16;
-        a2d_tmp = 10;
-        if (a2d_tmp == 0) begin
-            a2d_aac_pkd_state <= 0;
-            a2d_cnt <= 0;
-        end
-        else begin
-            a2d_aac_pkd_state <= 1;
-            a2d_cnt <= a2d_tmp;
-        end
-    end
-    else if (`AFC_TOP.st_curr == 12) begin
-        if (a2d_cnt == 0) begin
-            a2d_aac_pkd_state <= 0;
-            a2d_cnt <= 0;
-        end
-        else begin
-            a2d_cnt <= a2d_cnt - 1;
-        end
-    end
 
 // fsdb
 `ifdef DUMP_FSDB
@@ -201,16 +94,13 @@ end
 initial begin
     clk = 0;
     rstn = 1;
-    logen_rstn = 1;
     fork
         // rstn
         begin
             #50;
             rstn = 0;
-            logen_rstn = 0;
             #100;
             rstn = 1;
-            logen_rstn = 1;
         end
         // clk
         begin
@@ -224,189 +114,102 @@ end
 task sys_init;
     begin
         case_num                = 0;
-        chk_cnt                 = 0;
+        cmp_cnt                 = 0;
         err_cnt                 = 0;
-        afc_cg_auto             = 0;
-        afc_start               = 0;
-        cdb_afc_start           = 0;
-        cdb_bypass              = 1;
-        trx                     = 0;
-        divr                    = 0;
-        rg_forceband_en         = 0;
-        rg_vco_capband          = 0;
-        rg_afc_vcostable_time   = 0;
-        rg_afc_cnt_time         = 0;
-        a2d_afc_ncntr           = 0;
-        rg_aac_bypass           = 0;
-        rg_aac_stable_time      = 0;
-        rg_aac_cbandrange       = 0;
-        rg_ini_ibsel_rx         = 0;
-        rg_ini_ibsel_tx         = 0;
-        rg_aac_vrefsel_tx       = 1;
-        rg_aac_vrefsel_rx       = 2;
-        a2d_aac_pkd_state       = 0;
-        // logen
-        logen_start             = 0;
-        rg_logen_cal_bypass     = 0;
-        rg_logen_cnt_sel        = 3;
-        rg_logen_vsel_man       = 7;
-        rg_logen_vsel_seg0      = 1;
-        rg_logen_vsel_seg1      = 2;
-        rg_logen_vsel_seg2      = 3;
-        rg_logen_vsel_seg3      = 4;
-        rg_logen_vsel_seg4      = 5;
-        rg_logen_cntr_bound0    = 10;
-        rg_logen_cntr_bound1    = 20;
-        rg_logen_cntr_bound2    = 30;
-        rg_logen_cntr_bound3    = 40;
+        tx_buf_vld              = 0;
+        tx_buf_byte             = 0;
+        rx_buf_vld              = 0;
+        spi_en                  = 0;
+        spi_start_pulse         = 0;
+        tx_rx_seq               = 0;
+        tx_len                  = 0;
+        rx_len                  = 0;
+        clk_div                 = 0;
+        cpha                    = 0;
+        cpol                    = 0;
+        rx_adj_en               = 0;
+        rx_adj_clk              = 0;
+        ncs_dly                 = 0;
     end
 endtask
 
 task main_loop;
     integer fp, ret, i, j, k, tmp;
     begin
-        // openfile
-        fp = $fopen(`FILE_CASE_LIST, "r");
-        // case loop
-        begin: CASE_LOOP
-            while(1) begin
-                ret = $fscanf(fp, "%s", log_dir);
-                if (ret != 1) begin
-                    $display("%t, CASE_LIST FILE: %s, read end, simulation finish!", $time, `FILE_CASE_LIST);
-                    disable CASE_LOOP;
-                end
-                //$display("%t, CASE_LIST FILE: %s, ret: %d, log_dir: %s", $time, `FILE_CASE_LIST, ret, log_dir);
-                if ((case_num >= `CASE_START) && (case_num <= `CASE_STOP)) begin
-                    // load_cfg
-                    load_cfg;
-                    load_ncntr;
-                    afc_trigger;
-                    repeat(10) @(posedge clk);
-                    fork
-                        afc_check;
-                        ndec_check;
-                    join
-                    #200;
-                end
-                case_num = case_num + 1;
+        case1(0, 0);
+        case1(0, 1);
+        case1(1, 0);
+        case1(1, 1);
+    end
+endtask
+
+task case1;
+    input cpha_i;
+    input cpol_i;
+    begin
+        // delay
+        fork
+            // driv
+            begin
+                spi_en                  = 1;
+                #100;
+                tx_rx_seq               = 0;
+                tx_len                  = 1;
+                rx_len                  = 0;
+                tx_buf_byte             = 0;
+                tx_buf_vld              = 0;
+                rx_buf_vld              = 0;
+                clk_div                 = 0;
+                cpha                    = cpha_i;
+                cpol                    = cpol_i;
+                rx_adj_en               = 0;
+                rx_adj_clk              = 0;
+                ncs_dly                 = 2;
             end
-        end
-        // close file
-        $fclose(fp);
-    end
-endtask
-
-task afc_trigger;
-    begin
-        @(posedge clk);
-        afc_start = 1;
-        repeat(10) @(posedge clk);
-        afc_start = 0;
-    end
-endtask
-
-task load_cfg;
-    integer fp, ret, i, j, k, tmp;
-    reg [32*8-1:0] str;
-    begin
-        fp = $fopen({log_dir, "/", `FILE_CFG}, "r");
-        ret = $fscanf(fp, "%s %d", str, target_vco_capband);
-        ret = $fscanf(fp, "%s %d", str, trx);
-        ret = $fscanf(fp, "%s %x", str, divr);
-        ret = $fscanf(fp, "%s %d", str, afc_cg_auto);
-        ret = $fscanf(fp, "%s %d", str, rg_forceband_en);
-        ret = $fscanf(fp, "%s %d", str, rg_vco_capband);
-        ret = $fscanf(fp, "%s %d", str, rg_afc_vcostable_time);
-        ret = $fscanf(fp, "%s %d", str, rg_afc_cnt_time);
-        ret = $fscanf(fp, "%s %d", str, rg_aac_bypass);
-        ret = $fscanf(fp, "%s %d", str, rg_aac_stable_time);
-        ret = $fscanf(fp, "%s %d", str, rg_aac_cbandrange);
-        ret = $fscanf(fp, "%s %d", str, rg_ini_ibsel_rx);
-        ret = $fscanf(fp, "%s %d", str, rg_ini_ibsel_tx);
-        $fclose(fp);
-    end
-endtask
-
-task logen_trigger;
-    begin
-        repeat(10) @(posedge clk);
-        logen_start = 1;
-        repeat(2) @(posedge clk);
-        logen_start = 0;
-        repeat(10) @(posedge clk);
-    end
-endtask
-
-task logen_test;
-    begin
-        repeat(100) @(posedge clk);
-        logen_trigger;
-        repeat(500) @(posedge clk);
-    end
-endtask
-
-task load_ncntr;
-    begin
-        if (rg_afc_cnt_time == 18) begin
-            $readmemb({`LOG_DIR, "/", `NCNTR_M19}, ncntr_table);
-        end
-        else if (rg_afc_cnt_time == 63) begin
-            $readmemb({`LOG_DIR, "/", `NCNTR_M64}, ncntr_table);
-        end
-    end
-endtask
-
-task afc_check;
-    begin
-        begin: WAIT_FINISH
-            while(1) begin
+            // tx buf
+            begin
+                @(posedge spi_start_pulse);
+                repeat(10) @(posedge clk);
+                tx_buf_byte             = 8'h55;
+                tx_buf_vld              = 1;
+                @(posedge tx_buf_req);
                 @(posedge clk);
-                if (afc_finish) begin
-                    disable WAIT_FINISH;
-                end
+                tx_buf_vld              = 0;
             end
-        end
-        @(posedge clk);
-        if ((afc_vco_capband !== (target_vco_capband    )) &&       // jingdu wenti, may have one capband mistake!!!
-            (afc_vco_capband !== (target_vco_capband - 1)) &&
-            (afc_vco_capband !== (target_vco_capband + 1))) begin
-            err_cnt = err_cnt + 1;
-            $display("%t, case_num: %d, log_dir: %s", $time, case_num, log_dir);
-            $display("    afc_vco_capband check fail, afc: %d, log:%d", afc_vco_capband, target_vco_capband);
-        end
-        else begin
-            //$display("%t, case_num: %d, afc_vco_capband: %d, check pass.", $time, case_num, afc_vco_capband);
-        end
-        chk_cnt = chk_cnt + 1;
+            // rx buf
+            begin
+                @(posedge spi_start_pulse);
+                repeat(20) @(posedge clk);
+                rx_buf_vld              = 1;
+                @(posedge rx_buf_req);
+                @(posedge clk);
+                rx_buf_vld              = 0;
+            end
+            // finish
+            begin
+                @(posedge ncs);
+                #100;
+            end
+        join
     end
 endtask
 
-task ndec_check;
-    integer tmp;
-    reg [22:0] mult;
-    reg [13:0] chk;
+task trig;
     begin
-        mult = divr[15:0]*(rg_afc_cnt_time[6:0] + 1);
-        chk = mult[7] ? mult[21:8] + 1 : mult[21:8];
-        wait(`AFC_TOP.st_curr == 8);
-        if (`AFC_TOP.ndec_reg[13:0] !== chk[13:0]) begin
-            err_cnt = err_cnt + 1;
-            $display("%t, case_num: %d, log_dir: %s", $time, case_num, log_dir);
-            $display("    ndec check fail, afc: %04x, log:%04x", `AFC_TOP.ndec_reg[13:0], chk[13:0]);
-        end
-        else begin
-            //$display("%t, case_num: %d, ndec: %04x, check pass.", $time, case_num, chk[13:0]);
-        end
-
+        @(posedge clk);
+        spi_start_pulse = 1;
+        repeat(1) @(posedge clk);
+        spi_start_pulse = 0;
     end
 endtask
+
 
 task disp_sum;
     begin
         $display("---------------------------------------------------");
         $display("---------------------------------------------------");
         $display("---------------------------------------------------");
-        $display("  chk_cnt: %d", chk_cnt);
+        $display("  cmp_cnt: %d", cmp_cnt);
         if (err_cnt == 0) begin
             $display("      PASS.");
         end
@@ -417,5 +220,6 @@ task disp_sum;
         $display("---------------------------------------------------");
     end
 endtask
+
 endmodule
 
